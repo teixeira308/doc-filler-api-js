1b64ac43a24722d3bf5f0fc597e9099e07a84ee1
Guilherme Teixeira <teixeira308@hotmail.com>
2025-04-12 21:44:48 -0300

    add logs

--- controller/PessoaController.js
+++ controller/PessoaController.js
@@ -172,7 +172,7 @@ const importPessoasFromExcel = async (req, res) => {
             return res.status(400).json({ message: "Nenhum arquivo enviado" });
         }
         console.log(req.body);  // Verifique se o grupoId está aqui
-        console.log(req.file); 
+       
 
         const workbook = xlsx.read(req.file.buffer, { type: 'buffer' });
         const sheetName = workbook.SheetNames[0];
