e681024f8b2874383da9d5196841f5c07fe1d275
Guilherme Teixeira <teixeira308@hotmail.com>
2025-04-12 21:23:30 -0300

    fix listAll

--- controller/PessoaController.js
+++ controller/PessoaController.js
@@ -41,7 +41,15 @@ listAllPessoas = async (req, res) => {
         const offset = (page - 1) * pageSize;
         const totalPages = Math.ceil(totalCount[0].total / pageSize);
 
-        const [results] = await connection.query('SELECT p.*,g.nome as grupo_nome FROM pessoa p, grupo_pessoa g WHERE p.grupoId=g.id and p.userId=? order by createdAt desc LIMIT ? OFFSET ?', [req.userId, pageSize, offset]);
+        const [results] = await connection.query(
+            `SELECT p.*, g.nome AS grupo_nome
+             FROM pessoa p
+             LEFT JOIN grupo_pessoa g ON p.grupoId = g.id
+             WHERE p.userId = ?
+             ORDER BY p.createdAt DESC
+             LIMIT ? OFFSET ?`,
+            [req.userId, pageSize, offset]
+        );
         connection.release();
         var now = new Date();
         //Logmessage('Lista de pessoas recuperada do banco de dados:'+ results);
