f3b46b0cfec4d4721ac4f5742c0e10e06067c11a
Guilherme Teixeira <teixeira308@hotmail.com>
2025-04-14 11:51:43 -0300

    fix

--- routes/EpiRoutes.js
+++ routes/EpiRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { authenticateToken } = require('../middleware/auth');
-const EpiController = require('../controller/epiController');
+const EpiController = require('../controller/EpiController');
 
 // Rota para criar um novo EPI
 router.post('/epi', authenticateToken, EpiController.createEpi);
